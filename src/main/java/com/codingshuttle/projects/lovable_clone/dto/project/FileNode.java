b8606ad8654e9ac8e585ccc3497223fe4b515174
Anuj Kumar Sharma <anuj@titan.local>
2026-01-25 00:11:14 +0530

    Part - 12, Tool Calling and Advisor added

--- src/main/java/com/codingshuttle/projects/lovable_clone/dto/project/FileNode.java
+++ src/main/java/com/codingshuttle/projects/lovable_clone/dto/project/FileNode.java
@@ -3,4 +3,9 @@ package com.codingshuttle.projects.lovable_clone.dto.project;
 public record FileNode(
         String path
 ) {
+
+    @Override
+    public String toString() {
+        return path;
+    }
 }
